a8e973e472f50c44d636392eba47daff54d13892
admintran <duong170226@gmail.com>
2024-11-27 15:45:25 +0700

    Update Ss8_bai 1.cpp

--- Ss8_bai 1.cpp
+++ Ss8_bai 1.cpp
@@ -2,7 +2,7 @@
 
 int main(){
 	int a[5]={4,7,9,2,7};
-	printf("Cac phn tu trong mang la :");
+	printf("Cac phan tu trong mang la :");
 	for(int i=sizeof(a)/sizeof(int)-1;i>=0;i--){
 		printf("%d ",a[i]);
 	}
